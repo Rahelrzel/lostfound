287792f5ba5468c6dbf211e9ea219d50f2a388cc
getch-27 <getahunlast@gmail.com>
2026-09-19 14:23:21 +0300

    user reference

--- backend/src/routes/reportsRoute.ts
+++ backend/src/routes/reportsRoute.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import { CreateReport, GetReports, GetReportById, DeleteReport, UpdateReport } from "../controllers/ReportController";
+import { authenticateUser } from "../middleware/auth.moddleware";
 
 const router = express.Router();
 
 //create
-router.post("/", CreateReport);
+router.post("/" ,authenticateUser,CreateReport);
 //get reports
 router.get("/", GetReports);
 
-router.delete("/:id", DeleteReport)
-router.put("/:id", UpdateReport)
+router.delete("/:id",authenticateUser, DeleteReport)
+router.put("/:id",authenticateUser, UpdateReport)
 router.get("/:id", GetReportById)
 
 export default router;
